5d0624bfb5a88198a74af088e5b535da128a5704
Andrew Jarcho <andrew.jarcho@gmail.com>
2024-11-22 09:48:25 -0500

    Remove custom pytest switch
    On branch refactor_journal
    Changes to be committed:
        modified:   tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,38 +1,9 @@
 import pytest
 import logging
-import sys
-
-
-def pytest_addoption(parser):
-    """Add custom output control options."""
-    parser.addoption(
-        "--show-logs",
-        action="store_true",
-        default=False,
-        help="Show logger output"
-    )
 
 
 @pytest.fixture(autouse=True)
-def control_output(request, caplog):
-    """Control debug output for tests."""
-    # Create a console handler
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
-
-    # Get the root logger and add our handler
-    root_logger = logging.getLogger()
-    root_logger.addHandler(console_handler)
-
-    # Set levels based on --show-logs option
-    if request.config.getoption("--show-logs"):
-        root_logger.setLevel(logging.DEBUG)
-        console_handler.setLevel(logging.DEBUG)
-    else:
-        root_logger.setLevel(logging.WARNING)
-        console_handler.setLevel(logging.WARNING)
-
+def control_output(caplog):
+    """Set default logging level."""
+    caplog.set_level(logging.WARNING)
     yield
-
-    # Clean up
-    root_logger.removeHandler(console_handler)
