640ed319192581940a8c16fc9209c6fc494b0190
Andrew Jarcho <andrew.jarcho@gmail.com>
2024-11-22 08:00:12 -0500

    Remove ineffective code
    On branch refactor_journal
    Changes to be committed:
        modified:   tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,10 @@
 import pytest
 import logging
-import sys
 
 
 def pytest_addoption(parser):
     """Add custom output control options."""
-    group = parser.getgroup('output_control')
-
-    group.addoption(
-        "--show-prints",
-        action="store_true",
-        default=False,
-        help="Show print debug statements"
-    )
-
-    group.addoption(
+    parser.addoption(
         "--show-logs",
         action="store_true",
         default=False,
@@ -22,26 +12,9 @@ def pytest_addoption(parser):
     )
 
 
-class PrintInterceptor:
-    def __init__(self, old_stdout, show_prints):
-        self.old_stdout = old_stdout
-        self.show_prints = show_prints
-
-    def write(self, message):
-        if 'DEBUG' in message:
-            if self.show_prints:
-                self.old_stdout.write(message)
-        else:
-            self.old_stdout.write(message)
-
-    def flush(self):
-        self.old_stdout.flush()
-
-
 @pytest.fixture(autouse=True)
 def control_output(request, caplog):
     """Control debug output for tests."""
-    show_prints = request.config.getoption("--show-prints")
     show_logs = request.config.getoption("--show-logs")
 
     # Set up logging level
@@ -50,11 +23,4 @@ def control_output(request, caplog):
     else:
         caplog.set_level(logging.WARNING)
 
-    # Intercept print statements
-    old_stdout = sys.stdout
-    sys.stdout = PrintInterceptor(old_stdout, show_prints)
-
     yield
-
-    # Restore stdout
-    sys.stdout = old_stdout
